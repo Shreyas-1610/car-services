c8beb1e5a6c775b0f8467b891021be2735257d8b
Shreyas Kulkarni <kshreyas93241@gmail.com>
2024-07-28 19:05:49 +0530

    Add files via upload

---
+++ crs/src/com/shreyas/crs/CRSEntry.java
@@ -0,0 +1,33 @@
+package com.shreyas.crs;
+
+public class CRSEntry {
+    public static void main(String[] args) {
+        Car toyotaCar = new Car();
+        toyotaCar.setCarId("CAR-1");
+        toyotaCar.setBrand("Toyota");
+        toyotaCar.setModel("Fortuner");
+        toyotaCar.setNoOfAvailableCar(1);
+        toyotaCar.setPricePerDay(3000);
+
+        Car tataCar = new Car();
+        tataCar.setCarId("CAR-2");
+        tataCar.setBrand("Tata");
+        tataCar.setModel("Safari");
+        tataCar.setNoOfAvailableCar(2);
+        tataCar.setPricePerDay(2500);
+
+        Car mahindraCar = new Car();
+        mahindraCar.setCarId("CAR-3");
+        mahindraCar.setBrand("Mahindra");
+        mahindraCar.setModel("XUV 700");
+        mahindraCar.setNoOfAvailableCar(2);
+        mahindraCar.setPricePerDay(3500);
+
+        CarRentalService carRentalService = new CarRentalService();
+        carRentalService.addCars(toyotaCar);
+        carRentalService.addCars(tataCar);
+        carRentalService.addCars(mahindraCar);
+
+        carRentalService.options();
+    }
+}
